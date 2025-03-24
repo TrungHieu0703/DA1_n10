0e8d007942ce1dab8a6b85f8f7775b7b7a745314
huytran2225 <huyquangtran22@gmail.com>
2025-03-24 13:36:39 +0700

    First commit - Đẩy code lần đầu lên GitHub

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+// Đây là file chạy chính (Là nơi chúng require các file)
+require_once "./common/env.php";    // Chứa các biến môi trường
+require_once "./common/function.php";   // Chứa các hàm dùng chung
+
+// require các controller mà route trỏ tới
+
+// require Các model mà controller muốn sử dụng
+require_once './models/User.php';
+
+// Route (Điều hướng)
+$act = $_GET['act'] ?? '/';
+
+match ($act) {
+    // Nơi khai báo các đường dẫn
+    '/' => (new HomeController())->index(),
+
+    // Khai báo route
+};
