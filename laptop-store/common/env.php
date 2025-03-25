1134b4740a2d5350a913bb6a5cd02966ce749b9c
huytran2225 <huyquangtran22@gmail.com>
2025-03-25 22:25:00 +0700

    Thêm file mới

---
+++ laptop-store/common/env.php
@@ -0,0 +1,14 @@
+<?php
+// File chứa các biến muôi trường trong hệ thống
+// Khai báo các biến dưới dạng HẰNG SỐ để ko phải sử dụng $GLOBALS
+
+define('BASE_URL' , 'http://localhost/du_an_1/mvc-oop-basic_thi/');
+define('BASE_URL_ADMIN' , 'http://localhost/du_an_1/mvc-oop-basic_thi/admin/');
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'laptop_store'); // Tên database
+
+define('PATH_ROOT', __DIR__ . '/../');
